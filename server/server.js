cdcb1909ea956a8aa9834911f6c2b92d80758380
Naween Samad <naweensamad2019@gmail.com>
2026-08-11 16:53:24 +1000

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -18,8 +18,10 @@ app.get('/', (req, res) => {
     res.json({ message: 'API is running'})
 })
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000')
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
 
 
